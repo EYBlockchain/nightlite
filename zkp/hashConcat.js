3aede91d06f550bce8bd7268c9417a31ac7626cc
Will Kim <william.kim@ey.com>
2019-09-06 11:18:38 -0400

    feat: add hashConcat

---
+++ zkp/hashConcat.js
@@ -0,0 +1,33 @@
+const config = require('config');
+const utils = require('./utils');
+
+/**
+ * Converts array of into array of Buffers into a single buffer,
+ *
+ * TODO: What's the difference between this and hashC?
+ *
+ * - convert each item in items to a 'buffer' of bytes (2 hex values), convert those bytes into decimal representation
+ * - 'concat' each decimally-represented byte together into 'concatenated bytes'
+ * - hash the 'buffer' of 'concatenated bytes' (sha256) (sha256 returns a hex output)
+ * - truncate the result to the right-most 64 bits
+ *
+ * createHash: we're creating a sha256 hash
+ * update: [input string to hash (an array of bytes (in decimal representaion) [byte, byte, ..., byte] which represents the result of: item1, item2, item3. Note, we're calculating hash(item1, item2, item3) ultimately]
+ * digest: [output format ("hex" in our case)]
+ * slice: [begin value] outputs the items in the array on and after the 'begin value'
+ *
+ * @param {Array} items - Array of items to be hashed.
+ * @returns {String}
+ */
+function hashConcat(...items) {
+  const concatValue = utils.concatHexToSingleString(items);
+
+  const hash = `0x${crypto
+    .createHash('sha256')
+    .update(concatValue, 'hex')
+    .digest('hex')
+    .slice(-(config.get('hashLength') * 2))}`;
+  return hash;
+}
+
+module.exports = hashConcat;
