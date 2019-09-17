762707cde45ad87d5592df878389bfb3ea655d5f
Will Kim <william.kim@ey.com>
2019-09-17 14:13:09 -0400

    refactor: add zokrates signatures, haven't imported zokrates yet

--- nft/mint.js
+++ nft/mint.js
@@ -13,22 +13,23 @@ const zokrates = null;
  * @param {string} tokenId - the asset token
  * @param {string} ownerPublicKey - Address of the token owner
  * @param {string} salt - Alice's token serial number as a hex string
- * @param {string} account - Account that sends the transaction.
  * @param {Object} vkId - vkId for NFT's MintToken
- * @returns {string} z_A - The token
- * This is a convenience because the sender (Alice)
- * knows S_A,pk_A,n and n so could in fact calculate the token themselves.
- * @returns {Integer} z_A_index - the index of the token within the Merkle Tree.  This is required for later transfers/joins so that Alice knows which 'chunks' of the Merkle Tree she needs to 'get' from the NFTokenShield contract in order to calculate a path.
+ * @param {Object} blockchainOptions
+ * @param {String} blockchainOptions.nfTokenShieldJson - ABI of nfTokenShield
+ * @param {String} blockchainOptions.nfTokenShieldAddress - Address of deployed nfTokenShieldContract
+ * @param {String} blockchainOptions.account - Account that is sending htese transactions
+ * @param {Object} zokratesOptions
+ * @param {String} zokratesOptions.codePath - Location of compiled code (without the .code suffix)
+ * @param {String} [zokratesOptions.outputDirectory=./] - Directory to output all generated files
+ * @param {String} [zokratesOptions.witnessName=witness] - Name of witness file
+ * @param {String} [zokratesOptions.pkPath] - Location of the proving key file
+ * @param {Boolean} zokratesOptions.createProofJson - Whether or not to create a proof.json file
+ * @param {String} [zokratesOptions.proofName=proof.json] - Name of generated proof JSON.
+ * @returns {String} tokenCommitmentSender
+ * @returns {Number} senderCommitmentTreeIndex - the index of the token within the Merkle Tree.  This is required for later transfers/joins so that Alice knows which 'chunks' of the Merkle Tree she needs to 'get' from the NFTokenShield contract in order to calculate a path.
  */
-async function mint(
-  tokenId,
-  ownerPublicKey,
-  salt,
-  account,
-  vkId,
-  nfTokenShieldJson,
-  nfTokenShieldAddress,
-) {
+async function mint(tokenId, ownerPublicKey, salt, vkId, blockchainOptions, zokratesOptions) {
+  const { nfTokenShieldJson, nfTokenShieldAddress, account } = blockchainOptions;
   const nfTokenShield = ethersUtils.getContractWithSigner(
     nfTokenShieldJson,
     nfTokenShieldAddress,
@@ -42,25 +43,33 @@ async function mint(
     salt,
   );
 
-  const inputs = computeVectors([
-    new Element(tokenId, 'field'),
-    new Element(tokenCommitmentSender, 'field'),
-  ]);
+  const {
+    codePath,
+    outputDirectory,
+    witnessName,
+    pkPath,
+    provingScheme,
+    createProofJson,
+    proofName,
+  } = zokratesOptions;
 
-  const hostDir = config.NFT_MINT_DIR;
-
-  // compute the proof
-  // TODO: Integrate new zokrates.
-  let proof = await zokrates.computeProof(
-    [
+  await zokrates.computeWitness(
+    codePath,
+    outputDirectory,
+    witnessName,
+    computeVectors([
       new Element(tokenId, 'field'),
       new Element(ownerPublicKey, 'field'),
       new Element(salt, 'field'),
       new Element(tokenCommitmentSender, 'field'),
-    ],
-    hostDir,
-    'MintToken',
+    ]),
   );
+
+  let proof = await zokrates.generateProof(pkPath, codePath, provingScheme, {
+    createFile: createProofJson,
+    directory: outputDirectory,
+    fileName: proofName,
+  });
 
   proof = Object.values(proof);
   // convert to flattened array:
@@ -76,7 +85,11 @@ async function mint(
   const nfTokenContract = ethersUtils.getContractWithSigner(nfTokenJson, nfTokenAddress, account);
   await nfTokenContract.approve(nfTokenShield.address); // Correct reference for address?
 
-  // Mint a commitment. (Commitmint, if you will).
+  // Mint a commitment on the token shield contract.
+  const inputs = computeVectors([
+    new Element(tokenId, 'field'),
+    new Element(tokenCommitmentSender, 'field'),
+  ]);
   const finalInputs = [...inputs, '1']; // TODO: Not sure why the last 1 is necessary.
   const mintReceipt = await nfTokenShield.mint(proof, finalInputs, vkId);
   const { senderCommitmentTreeIndex } = mintReceipt.logs[0].args;
