74509dd08d1b134378c1533c061da9e7d26e334f
Mel-Marsollier <mmarsollier@outlook.com>
2019-10-08 21:29:38 -0700

    feat:ft-transfer

--- ft/transfer.js
+++ ft/transfer.js
@@ -1,3 +1,13 @@
+const ethersUtils = require('@eyblockchain/dapp-utils/ethers');
+const zokrates = require('@eyblockchain/zokrates.js');
+const fs = require('fs');
+const config = require('../config/config');
+const utils = require('../zkp/utils');
+const { recursiveHashConcat } = require('../index');
+const { hexToDecimal } = require('../zkp/utils/conversions');
+const computeVectors = require('../zkp/computeVectors');
+const Element = require('../zkp/Element');
+
 async function transfer() {
 
 
